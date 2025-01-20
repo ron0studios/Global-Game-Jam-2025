5a8291140fc6498f95244830fffd57434ab4cfff
Cole <coleharries06@gmail.com>
2025-01-20 19:50:59 +0000

    Basic player
    Added a player that can blow ;) and made the bubble fall slower

--- objects/bubble.gd
+++ objects/bubble.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var fall_speed = 150
 var prev_velocity
 signal hit
 
@@ -22,20 +23,12 @@ func hit_floor():
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += fall_speed * delta
 	else:
 		print("hit")
 		print(prev_velocity)
 		hit_floor()
 		velocity.y = -prev_velocity
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	prev_velocity = velocity.y # velocity before landing on floor
 	move_and_slide()
