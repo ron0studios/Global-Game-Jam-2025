9672da6617d0f13164bb880671bff84b48d980a1
hryspider <peaty-danish0s@icloud.com>
2025-01-21 13:20:56 +0000

    tweaking player movement
    Made movement more slippery. Different ground/air stats.

--- player.gd
+++ player.gd
@@ -1,14 +1,23 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const MAX_SPEED = 300.0
+const GROUND_ACCEL = 1500.0
+const GROUND_DECEL = 2000.0
+const AIR_ACCEL = 800.0
+const AIR_DECEL = 400.0
 const JUMP_VELOCITY = -400.0
+
 var breath = 100
 
 
 func _physics_process(delta: float) -> void:
+	var accel = GROUND_ACCEL
+	var decel = GROUND_DECEL
 	# Add the gravity.
 	if not is_on_floor():
+		accel = AIR_ACCEL
+		decel = AIR_DECEL
 		velocity += get_gravity() * delta
 
 	# Handle jump.
@@ -19,9 +28,13 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = clamp(velocity.x + direction * accel * delta, -MAX_SPEED, MAX_SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.x > 0:
+			velocity.x = max(0, velocity.x - decel*delta)
+		if velocity.x < 0:
+			velocity.x = min(0, velocity.x + decel*delta)
+	
 	
 	if Input.is_action_pressed("blow"): #Space key
 		breath = max(0, breath-1)
