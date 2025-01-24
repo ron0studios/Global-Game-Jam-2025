8d82c53d1d49f4c2a21c968fe87f7204fd952ef9
hryspider <peaty-danish0s@icloud.com>
2025-01-24 20:21:28 +0000

    state machine structure implemented

--- objects/buoyancy.gd
+++ objects/buoyancy.gd
@@ -3,4 +3,4 @@ extends Node
 @onready var parent = get_parent()
 func _process(delta: float) -> void:
 	if parent.position.y > Global.water_level:
-		parent.velocity.y += delta * parent.position.y-Global.water_level
+		parent.velocity.y = (parent.velocity.y + delta * parent.position.y-Global.water_level*0.4) * 0.8
