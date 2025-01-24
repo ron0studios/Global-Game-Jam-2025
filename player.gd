5902b8db45173aa685b6285c8528041ec41a555c
Ron0Studios <47331292+ron0studios@users.noreply.github.com>
2025-01-24 16:11:15 +0000

    tweaked player speed values
    faster acceleration, and controlled jumping

--- player.gd
+++ player.gd
@@ -3,11 +3,14 @@ extends CharacterBody2D
 @onready var blow_hbox = $BlowArea/CollisionShape2D
 
 const MAX_SPEED = 300.0
-const GROUND_ACCEL = 1500.0
-const GROUND_DECEL = 2000.0
-const AIR_ACCEL = 800.0
-const AIR_DECEL = 400.0
-const JUMP_VELOCITY = -400.0
+const GROUND_ACCEL = 1500000.0
+const GROUND_DECEL = 2000000.0
+const AIR_ACCEL = GROUND_ACCEL*0.7
+const AIR_DECEL = GROUND_ACCEL*0.7
+const JUMP_VELOCITY = -600.0
+const FALL_MULTIPLIER = 1.5 # how much faster to fall when jump key released
+var fall_factor = 1
+
 
 var breath = 100
 var blowing = false: #enables blowing hitbox when true
@@ -21,15 +24,22 @@ var blowing = false: #enables blowing hitbox when true
 func _physics_process(delta: float) -> void:
 	var accel = GROUND_ACCEL
 	var decel = GROUND_DECEL
+	
 	# Add the gravity.
 	if not is_on_floor():
 		accel = AIR_ACCEL
 		decel = AIR_DECEL
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * fall_factor
+	else:
+		fall_factor = 1
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_released("jump") and not is_on_floor():
+		fall_factor = FALL_MULTIPLIER
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
