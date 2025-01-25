92ab49f699eb40c91465c3b4b0dbc3def6ba880a
hryspider <peaty-danish0s@icloud.com>
2025-01-25 17:04:28 +0000

    New bubble system (0 health upwards)

--- objects/bubble.gd
+++ objects/bubble.gd
@@ -1,7 +1,9 @@
 extends RigidBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-var hp = 100
+var health = 0:
+	set(value):
+		health = max(0, value)
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -18,16 +20,15 @@ func hit_floor():
 	#$Sprite2D.scale = Vector2(1.2,1.2)
 	$CPUParticles2D.emitting = true
 	hit.emit()
-	pass
 
 func _physics_process(delta: float) -> void:
 	if position.y > Global.water_level:
-		hp -= delta * 10
+		health -= delta * 10
 		apply_impulse(Vector2.UP * (position.y-Global.water_level) * 0.1)
-	$Label.text = str(hp)
+	$Label.text = str(health)
 	rotation = 0
-	modulate = Color.RED.lerp(Color.WHITE, hp/100.0)
-	
+	scale = Vector2.ONE * (0.5+health/30)
+	print(animated_sprite_2d.material.shader)
 
 
 func _on_body_entered(body: Node) -> void:
@@ -39,5 +40,5 @@ func _on_body_entered(body: Node) -> void:
 func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
 	print(body, body.is_in_group("player"), body.position.y)
 	if body.is_in_group("player") and body.position.y > position.y+20:
-		hp -= 30
+		health -= 30
 		linear_velocity.y = min(-300, linear_velocity.y)
