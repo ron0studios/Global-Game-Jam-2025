73102e853011926d5f683695828e62bf9380cda7
hryspider <peaty-danish0s@icloud.com>
2025-01-25 16:34:13 +0000

    New bubble courtesy of Dan

--- objects/bubble.gd
+++ objects/bubble.gd
@@ -1,4 +1,5 @@
 extends RigidBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var hp = 100
 
@@ -9,15 +10,13 @@ var prev_velocity
 var base_scale = Vector2.ONE*0.5
 signal hit
 
+func _ready() -> void:
+	animated_sprite_2d.play("default")
+
 func hit_floor():
 	var wobble = get_tree().create_tween().set_trans(Tween.TRANS_BOUNCE)
 	#$Sprite2D.scale = Vector2(1.2,1.2)
 	$CPUParticles2D.emitting = true
-	wobble.tween_property($Sprite2D,"scale", base_scale*1.1, 0.1)
-	wobble.tween_property($Sprite2D,"scale", base_scale*0.9, 0.1)
-	wobble.tween_property($Sprite2D,"scale", base_scale*1.05, 0.1)
-	wobble.tween_property($Sprite2D,"scale", base_scale, 0.2)
-	wobble.tween_callback(func(): $Sprite2D.scale = base_scale)
 	hit.emit()
 	pass
 
