65870909161d0d1a28db02a2fdf8a260bc224dca
AMgr8 <153189709+AMgr8@users.noreply.github.com>
2025-01-25 16:38:05 +0000

    Update camera_2d.gd
    fixed the x position of camera, gave limits to the zoom and y movement

--- objects/camera_2d.gd
+++ objects/camera_2d.gd
@@ -3,13 +3,21 @@ extends Camera2D
 
 @export var targets : Array[NodePath]
 
+
 func _process(delta: float) -> void:
 	var avg_pos = Vector2.ZERO
 	var max_dist = 0
+	var min_dist = 350
 	for i in targets:
-		avg_pos += get_node(i).position
-	avg_pos /= len(targets)
-	for i in targets:
+		avg_pos.y += get_node(i).position.y
+		var dist = position.distance_to(get_node(i).position)
 		max_dist = max(max_dist, position.distance_to(get_node(i).position))
-	position = position.lerp(avg_pos, 1 - pow(delta, 0.1))
-	zoom = zoom.lerp(Vector2.ONE*700/max(200, max_dist), 1 - pow(delta, 0.2))
+		
+		
+	avg_pos /= len(targets)
+	if avg_pos.y < -100:
+		avg_pos.y = -100
+	position = position.lerp(avg_pos, 1 - pow(delta, 0.3))
+	if max_dist > 500:
+		max_dist = 500
+	zoom = zoom.lerp(Vector2.ONE*700/max(min_dist, max_dist), 1 - pow(delta, 0.3))
