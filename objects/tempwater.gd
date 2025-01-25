9c9f0438961a7caca82373b1b22c338a05d05057
Indigo Nolan <indigogonolan@gmail.com>
2025-01-25 19:47:32 +0000

    bathtub water

--- objects/tempwater.gd
+++ objects/tempwater.gd
@@ -1,7 +1,8 @@
 @tool
-extends ColorRect
+extends Polygon2D
 
 #Delete this script if you like
 
 func _process(delta: float) -> void:
-	position.y = Global.water_level
+	#position.y = Global.water_level
+	pass
