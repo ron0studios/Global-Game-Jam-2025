c57402c7554a06989e3a1ebe6360ebf057618471
hryspider <peaty-danish0s@icloud.com>
2025-01-25 16:00:57 +0000

    bubble health system!
    big things gone changed

--- objects/bubble.gd
+++ objects/bubble.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+var hp = 100
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -22,10 +23,22 @@ func hit_floor():
 
 func _physics_process(delta: float) -> void:
 	if position.y > Global.water_level:
+		hp -= delta * 10
 		apply_impulse(Vector2.UP * (position.y-Global.water_level) * 0.1)
+	$Label.text = str(hp)
+	rotation = 0
+	modulate = Color.RED.lerp(Color.WHITE, hp/100.0)
 	
 
 
 func _on_body_entered(body: Node) -> void:
+	
 	if body.name == "StaticBody2D":
 		hit_floor()
+
+
+func _on_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+	print(body, body.is_in_group("player"), body.position.y)
+	if body.is_in_group("player") and body.position.y > position.y+20:
+		hp -= 30
+		linear_velocity.y = min(-300, linear_velocity.y)
