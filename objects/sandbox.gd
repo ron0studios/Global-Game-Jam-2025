1f60c9e4ca62d4b58597a5b52e35e74ef6e3c6c9
AMgr8 <153189709+AMgr8@users.noreply.github.com>
2025-01-26 00:34:43 +0000

    increase spwan rate

--- objects/sandbox.gd
+++ objects/sandbox.gd
@@ -6,15 +6,18 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	make_bubble()
 	pass # Replace with function body.
 
 func _on_timer_timeout() -> void:
 	if randi_range(0,2) == 0:
-		make_bubble()
+		for i in randi_range(1,2):
+			make_bubble()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func make_bubble():
 	var bubble_inst = bubble_scene.instantiate()
 	bubble_inst.health = 1
 	bubble_inst.position.x = randi_range(-200,200)
+	bubble_inst.position.y = -700
 	add_child(bubble_inst)
