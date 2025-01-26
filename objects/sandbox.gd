3c4d2bc9127f78cf76ec830f7a683b64ab110cac
AMgr8 <153189709+AMgr8@users.noreply.github.com>
2025-01-26 00:18:48 +0000

    bubble spwanign

--- objects/sandbox.gd
+++ objects/sandbox.gd
@@ -2,11 +2,19 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
+@onready var bubble_scene = preload("res://objects/bubble.tscn")
+
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _on_timer_timeout() -> void:
+	if randi_range(0,2) == 0:
+		make_bubble()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	$Water.apply_force($Player.global_position, 128.0 * Vector2.DOWN, 10.0)
-	pass
+func make_bubble():
+	var bubble_inst = bubble_scene.instantiate()
+	bubble_inst.health = 1
+	bubble_inst.position.x = randi_range(-200,200)
+	add_child(bubble_inst)
