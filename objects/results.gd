94f5e96c5ce5f5ca8ffa588f8665ef685b9645aa
AMgr8 <153189709+AMgr8@users.noreply.github.com>
2025-01-26 15:03:24 +0000

    adds scores

--- objects/results.gd
+++ objects/results.gd
@@ -28,7 +28,6 @@ func enable_graph():
 	queue_redraw()
 	print("H")
 	for i in range(1,(Global.num_of_players+1)):
-		print("massive horse cock should be there")
 		var froth_inst = froth.instantiate()
 		froth_inst.position =Vector2((i+0.5)*150, 900 - (700*Global.results[i-1]/float(best)))
 		froth_inst.play("default")
@@ -45,5 +44,15 @@ func _draw():
 			var col = Global.player_colors[i-1]
 			if i>Global.num_of_players:
 				col = Color(0.3, 0.3, 0.3)
+			else:
+				var l = Label.new()
+				l.text = str(Global.results[i - 1])
+				l.position.x = i*150 + 75
+				l.position.y = 900 + 30
+				l.vertical_alignment = VERTICAL_ALIGNMENT_TOP
+				l.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+				l.add_theme_font_override("font",load("res://fonts/Kenney Pixel.ttf"))
+				l.add_theme_font_size_override("font_size",50)
+				add_child(l)
 			draw_rect(Rect2(Vector2(i*150, 900), Vector2(150, -700)), col.darkened(0.7))
 			draw_rect(Rect2(Vector2(i*150, 900), Vector2(150, -700*(Global.results[i-1]/float(best)))), col)
