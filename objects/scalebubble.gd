3ff09981bc7b6e26ab15266d4d4038610d7124bf
AMgr8 <153189709+AMgr8@users.noreply.github.com>
2025-01-26 03:14:38 +0000

    Update scalebubble.gd
    made bubble scale logarithmic after score = 150

--- objects/scalebubble.gd
+++ objects/scalebubble.gd
@@ -3,4 +3,9 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	print()
-	scale = Vector2.ONE * (0.5+(get_parent().health/100.0))
+	var Bscale
+	if get_parent().health <= 150:
+		Bscale = 0.5 + get_parent().health/100.0
+	else:
+		Bscale = log(get_parent().health/1.5)
+	scale = Vector2.ONE * Bscale
