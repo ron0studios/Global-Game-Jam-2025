e75521c3aa885bb7ebed2797bcd9299fbc512b3b
AMgr8 <153189709+AMgr8@users.noreply.github.com>
2025-01-26 01:25:07 +0000

    bubble spawn rate fixed, health randomised

--- objects/sandbox.gd
+++ objects/sandbox.gd
@@ -11,14 +11,14 @@ func _ready() -> void:
 	make_bubble(-500) # Replace with function body.
 
 func _on_timer_timeout() -> void:
-	if randi_range(0,2) == 0:
+	if randi_range(0,3) == 0:
 		for i in randi_range(1,2):
 			make_bubble(-700)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func make_bubble(start_y):
 	var bubble_inst = bubble_scene.instantiate()
-	bubble_inst.health = 1
+	bubble_inst.health = randi_range(1,40)
 	bubble_inst.position.x = randi_range(-200,200)
 	bubble_inst.position.y = start_y
 	add_child(bubble_inst)
