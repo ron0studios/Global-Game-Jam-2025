abdf7a5fa40f75feaf382a5f747949cf2e135268
AMgr8 <153189709+AMgr8@users.noreply.github.com>
2025-01-27 15:07:04 +0000

    changed scaling of bubbles slightly
    makes bubbles scale slower but the base scale is higher

--- objects/scalebubble.gd
+++ objects/scalebubble.gd
@@ -6,5 +6,5 @@ func _process(delta: float) -> void:
 	#if get_parent().health <= 150:
 		#Bscale = 0.5 + get_parent().health/100.0
 	#else:
-	Bscale = 0.5 + log(get_parent().health/100.0 + 1)
+	Bscale = 0.65 + log(get_parent().health/120.0 + 1)
 	scale = Vector2.ONE * Bscale
